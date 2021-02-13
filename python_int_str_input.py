47234678985871d4cfe24a9c7edbe9e63a0e9fb5
amankumarsingh9 <78997002+amankumarsingh9@users.noreply.github.com>
2021-02-13 12:12:28 +0530

    Add files via upload

---
+++ python_int_str_input.py
@@ -0,0 +1,17 @@
+##a=55
+##b=60
+##c="Aurangabad"
+##print(a+b)
+##print(c)
+##
+##
+##a=int(input())
+##b=int(input())
+##print(a+b)
+
+country_name=input("enter your country name: ")
+state_name =input("enter state: ")
+pin_code=int(input("enter pincode: "))
+print(f"Your country name is: {country_name}")
+print(f"Your state name is: {state_name}")
+print(f"Your pincode is: {pin_code}")
